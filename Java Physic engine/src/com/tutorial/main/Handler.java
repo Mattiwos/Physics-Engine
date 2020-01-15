3790f6727102b07d8e14480a16d73bbbaf24625f
Mattiwos <bmattiwos@gmail.com>
2020-01-15 09:23:28 -0800

    fixed array of object names to correct one

--- Java Physic engine/src/com/tutorial/main/Handler.java
+++ Java Physic engine/src/com/tutorial/main/Handler.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 
 public class Handler { //maintanes and updates all objects
     //will loop throgh all objects
-    LinkedList<GameObject> objects = new LinkedList<GameObject>();
+    LinkedList<GameObject> object = new LinkedList<GameObject>();
 
     public void tick(){
         for(int i = 0; i < object.size(); i++){ //loops through all obejcts
@@ -14,7 +14,7 @@ public class Handler { //maintanes and updates all objects
         }
     }
     public void render(Graphics g){
-        for(int i =0; i < objects.size(); i++){
+        for(int i =0; i < object.size(); i++){
             GameObject tempObject = object.get(i);
             tempObject.render(g); //renders all objects
         }
